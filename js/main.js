30e8f38820d5709609ec43f0030aa35d14c0c3a6
Jade Dominguez <plusjade@gmail.com>
2015-01-10 17:55:34 -0800

    Add click handlers for when user clicks paper and scissors buttons.

--- js/main.js
+++ js/main.js
@@ -3,5 +3,11 @@ document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('rock').onclick = function() {
         console.log('rock click');
     }
+    document.getElementById('paper').onclick = function() {
+        console.log('paper click');
+    }
+    document.getElementById('scissors').onclick = function() {
+        console.log('scissors click');
+    }
 
 })
