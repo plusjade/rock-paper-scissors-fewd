f61992511bd1a53eefad140fcb00bf3f36bb8d5b
Jade Dominguez <plusjade@gmail.com>
2015-01-10 17:59:04 -0800

    Add status updates for when user clicks  paper and scissors.

--- js/main.js
+++ js/main.js
@@ -6,9 +6,11 @@ document.addEventListener('DOMContentLoaded', function(){
     }
     document.getElementById('paper').onclick = function() {
         console.log('paper click');
+        document.getElementById('status').innerHTML = 'You played paper. The bot played {TEST} . You win! :)';
     }
     document.getElementById('scissors').onclick = function() {
         console.log('scissors click');
+        document.getElementById('status').innerHTML = 'You played scissors. The bot played {TEST} . You win! :)';
     }
 
 })
