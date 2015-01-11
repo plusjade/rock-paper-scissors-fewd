33b6bd07a9d932b487f68d9b05a93042431e0a54
Jade Dominguez <plusjade@gmail.com>
2015-01-10 18:08:18 -0800

    Add very basic win/lose logic in rock condition only. Stub the botChoice.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,22 @@ document.addEventListener('DOMContentLoaded', function(){
 
     document.getElementById('rock').onclick = function() {
         console.log('rock click');
+
+        var botChoice = 'paper';
+
+        if(botChoice === 'rock') {
+            // TIE - rock ties with rock
+            console.log(botChoice, 'TIE');
+        }
+        else if(botChoice === 'paper') {
+            // LOSE - paper covers rock
+            console.log(botChoice, 'LOSE');
+        }
+        else if(botChoice === 'scissors') {
+            // WIN - rock breaks scissors
+            console.log(botChoice, 'WIN');
+        }
+
         document.getElementById('status').innerHTML = 'You played rock. The bot played {TEST} . You win! :)';
     }
     document.getElementById('paper').onclick = function() {
